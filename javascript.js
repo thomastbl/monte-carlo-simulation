cee227f1efa7756fa69763c78042a3b06cc6b20a
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-06-22 22:35:05 +0200

    refactor: wrap drawing logic into dedicated functions

--- javascript.js
+++ javascript.js
@@ -1,20 +1,38 @@
 const canvas = document.getElementById("grid");
 const ctx = canvas.getContext("2d");
 
-let mousePosition = {
-  x: 0,
-  y: 0,
-};
+let originMousePosition = null;
 
-ctx.beginPath();
-ctx.moveTo(mousePosition.x, mousePosition.y);
+function saveOriginPoint(event) {
+  originMousePosition = getMousePositionInCanvas(event);
+}
 
-canvas.addEventListener("click", (event) => {
+function getMousePositionInCanvas(event) {
+  let mousePosition = {
+    x: 0,
+    y: 0,
+  };
+
   const canvasData = canvas.getBoundingClientRect();
-
   mousePosition.x = event.clientX - canvasData.x;
   mousePosition.y = event.clientY - canvasData.y;
-  ctx.lineTo(mousePosition.x, mousePosition.y);
+  return mousePosition;
+}
 
+function draw(mousePosition, originMousePosition) {
+  ctx.beginPath();
+  ctx.moveTo(originMousePosition.x, originMousePosition.y);
+  ctx.lineTo(mousePosition.x, mousePosition.y);
   ctx.stroke();
+}
+
+canvas.addEventListener("click", (event) => {
+  let mousePosition = getMousePositionInCanvas(event);
+
+  if (originMousePosition === null) {
+    saveOriginPoint(event);
+  } else {
+    draw(mousePosition, originMousePosition);
+    originMousePosition = mousePosition;
+  }
 });
